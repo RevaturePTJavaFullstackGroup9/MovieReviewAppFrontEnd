9b4cbefe204b41bdda76415427319d6facd8d6be
Carlos Aguilar <77519735+ssbucarlos@users.noreply.github.com>
2025-11-03 15:51:12 -0600

    Implement JWT and session storage

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React from 'react';
 import Home from "./Home";
 import AdminMovies from "./AdminMovies";
 import MovieDetails from "./MovieDetails";
 import ReviewPage from "./components/ReviewPage/ReviewPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import SignupPage from "./components/LoginPage/SignupPage";
+import JwtContext from "./components/Context/JwtContext";
 
 /**
  * App entry with routing.
@@ -10,28 +14,47 @@ import ReviewPage from "./components/ReviewPage/ReviewPage";
  * Add more routes as your app grows.
  */
 export default function App() {
-  return (
-    <BrowserRouter>
+  // Create Contexts here
+  const [jwt, setJwt] = React.useState(() => {
+    return sessionStorage.getItem('jwt') ?? '';
+  });
+  const jwtContext = {jwt: jwt, setJwt: setJwt};
+  
+  React.useEffect(
+    () => {
+      sessionStorage.setItem('jwt', jwt);
+    }, [jwt]
+  ); 
 
-      <nav style={{padding:12, borderBottom:"1px solid #eee"}}>
+
+  return (
+    <JwtContext value={jwtContext}>
+      <BrowserRouter>
+
+        <nav style={{padding:12, borderBottom:"1px solid #eee"}}>
           <Link to="/" style={{marginRight:12}}>Home</Link>
           <Link to="/admin">Admin</Link>
+          <h5>{jwt}</h5>
         </nav>
 
-        
-      <Routes>
-        {/* Homepage route */}
-        <Route path="/" element={<Home />} />
+          
+        <Routes>
+          {/* Homepage route */}
+          <Route path="/" element={<Home />} />
 
-        <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route path="/movies/:id" element={<MovieDetails />} />
 
-        {/* Admin route */}
-        <Route path="/admin" element={<AdminMovies />} />
+          {/* Admin route */}
+          <Route path="/admin" element={<AdminMovies />} />
 
-        {/* Example: <Route path="/movies" element={<Movies />} /> */}
-        <Route path="/movie_reviews/:movieId" element={<ReviewPage/>} />
+          {/* Example: <Route path="/movies" element={<Movies />} /> */}
+          <Route path="/movie_reviews/:movieId" element={<ReviewPage/>} />
 
-      </Routes>
-    </BrowserRouter>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage/>}/>
+
+        </Routes>
+      </BrowserRouter>
+    </JwtContext>
   );
 }
---
+++ my-app/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+import JwtContext from '../Context/JwtContext';
+
+const handleSubmit = (event, username, password, setSubmissionStatus, setJwt, navigate) => {
+    event.preventDefault();
+
+    const loginRequest = {
+        "username": username,
+        "password": password
+    };
+
+    axios.post("http://localhost:8080/api/auth/login", loginRequest)
+        .then(
+            response => {
+                console.log(`Succesfully logged in! Response= ${JSON.stringify(response.data)}`)
+                // Set Auth Token Globally here
+                setJwt(response.data.token);
+                setSubmissionStatus("SUCCESS");
+                setTimeout( () => {navigate("/")}, 3000)
+            }
+        )
+        .catch(
+            error => {
+                if (error.response){
+                    console.error(`Server responded with an error. Raw Data=${JSON.stringify(error.response.data)}, status code: ${error.response.status}`)
+                }
+                else if (error.request){
+                    console.error(`No response received from server!`)
+                }
+                else{
+                    console.error(`Unexpected error occured during signin, error="${error}"`)
+                }
+            
+                setSubmissionStatus("FAIL")
+            }
+        )
+};
+
+const LoginMessageDisplay = (submissionStatus, jwt) => {
+    if (submissionStatus === "SUCCESS"){
+        return (<>
+            <h3> Sucessfully Login!</h3>
+            <h4> Now rerouting to the main page...</h4>
+        </>)
+    }
+    else if (submissionStatus === "FAIL"){
+        return (<>
+            <h3> Unable to Login...</h3>
+            <h4> Please double check your username and password and try again later...</h4>
+        </>
+        )
+    }
+
+    return (
+        <></>
+    )
+};
+
+const LoginPage = () => {
+    
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [submissionStatus, setSubmissionStatus] = React.useState('PENDING');
+    const {jwt, setJwt} = React.useContext(JwtContext);
+    const navigate = useNavigate();
+
+    return (<>
+        <form onSubmit={e => handleSubmit(e, username, password, setSubmissionStatus, setJwt, navigate)}>
+            <label htmlFor='usernameInput'>Username:</label>
+            <input type='text' value={username} onChange={e => setUsername(e.target.value)} /><br/>
+            <label htmlFor='passwordInput'>Password:</label>
+            <input type='password' value={password} onChange={e => setPassword(e.target.value)}/><br/>
+            <button type="submit">Login</button>            
+        </form>
+        {LoginMessageDisplay(submissionStatus, jwt)}
+        <h3>Not yet registered?</h3>
+        <h5>Please click  <a href='/signup'>here</a></h5>
+
+    </>)
+};
+
+export default LoginPage;
